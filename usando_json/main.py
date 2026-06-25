59d5dee59d1a0e85b0ad30a1095cc8191ef9a30f
NandoGonz <168700146+NandoGonz@users.noreply.github.com>
2026-06-24 20:48:49 -0500

    Fix typos in app.py and ejemplo.txt; implement inventory management system with classes and main menu; remove unused tkinter example files.

--- usando_json/main.py
+++ usando_json/main.py
@@ -1,12 +1,12 @@
 import json
 
-archivo_datos = "datos.json"
+ARCHIVO_DATOS = "datos.json"
 data = {"nombre": "Luis Fernando", "edad": 29, "profesión": "comerciante"}
 
-with open(archivo_datos, "w", encoding="UTF-8") as archivo:
+with open(ARCHIVO_DATOS, "w", encoding="UTF-8") as archivo:
     json.dump(data, archivo, indent=4)
 
-with open(archivo_datos, "r", encoding="UTF-8") as archivo:
+with open(ARCHIVO_DATOS, "r", encoding="UTF-8") as archivo:
     datos_recuperados = json.load(archivo)
 
 print(datos_recuperados)
@@ -14,17 +14,17 @@ print(datos_recuperados)
 # --------------------------------------------------#
 # primero leemos el archivo para poder modificarlo  #
 # --------------------------------------------------#
-with open(archivo_datos, "r", encoding="UTF-8") as archivo:
+with open(ARCHIVO_DATOS, "r", encoding="UTF-8") as archivo:
     datos = json.load(archivo)
 
 datos["edad"] = 30  # Modifica un valor numérico
 datos["items"] = "relojes"
 datos["nuevo_campo"] = True  # Añade una clave completamente nueva
 
-with open(archivo_datos, "w", encoding="UTF-8") as archivo:
+with open(ARCHIVO_DATOS, "w", encoding="UTF-8") as archivo:
     json.dump(datos, archivo, indent=4)
 
-with open(archivo_datos, "r", encoding="UTF-8") as archivo:
+with open(ARCHIVO_DATOS, "r", encoding="UTF-8") as archivo:
     mostrar_datos = json.load(archivo)
 
 print(mostrar_datos)
